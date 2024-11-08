58aa979f2e9daa57cc642120e545df85b883ce37
Nahir03 <hollycarla392@gmail.com>
2024-11-08 11:18:43 -0300

    Update FooterSection.jsx

--- torneo-videojuegos/src/secciones/Grupo 1 (Damián y Carla)/FooterSection.jsx
+++ torneo-videojuegos/src/secciones/Grupo 1 (Damián y Carla)/FooterSection.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const FooterSection = () => {
   return (
-    <div className='text-white h-[20vw] text-center bg-white bg-gradient-to-b from-black from-10% via-[#001e1b] via-30% to-[#00c0ad] to-100%' >
-      FooterSection
+    <div className='text-white flex justify-center pt-[4vw] h-[20vw] text-center bg-white bg-gradient-to-b from-black from-10% via-[#001e1b] via-30% to-[#00c0ad] to-100%' >
+      <ul className='flex text-white gap-[1.2vw] font-light mr-[6vw]'>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#infoTorneo">Información del torneo</a></li>
+        <li><a href="#cronograma">Cronograma</a></li>
+        <li><a href="#puntuaciones">Puntuaciones</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+      </ul>
     </div>
   )
 }
+
 
 export default FooterSection
