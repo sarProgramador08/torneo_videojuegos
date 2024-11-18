055631c939bca95b234d010e1987e9bfd79d8ac1
Pabloalfaro219 <juancito212016@gmail.com>
2024-11-18 12:14:33 -0300

    imagenes
    las partes en donde van las imagenes(falta agregarle las imagenes porque no pude sacarlas de fingma)

--- torneo-videojuegos/src/secciones/Grupo 2 (Pablo y Mika)/InfoSection.jsx
+++ torneo-videojuegos/src/secciones/Grupo 2 (Pablo y Mika)/InfoSection.jsx
@@ -5,10 +5,13 @@ const InfoSection = () => {
   return (
     <div> 
      <div> {/* explicacion breve del torneo */}
+      const <img src="" alt="redondo morado" />
       <h2 className='text-xl font-black text-left text-white'>¿Que es Sarmiento Gaming?</h2>
        <p className='text-sm font-light text-left text-white'>Sarmiento Gaming es un torneo (presencial) de videojuegos creado por alumnos de la Escuela Profesional Sarmiento de la oferta formativa programador turno mañana 2024, para estudiantes de la institución interesados. Se realizará el Lunes 11 y Martes 12 de noviembre de 2024 en horario de 9:00 a 11:45.</p>
+      const <img src="https://lh3.googleusercontent.com/drive-storage/AJQWtBPi6UtOI-wn2GocXpVF-F78BbVoMYft6C2T9LklfSm-Y8B-yH06IQ0WW6k__piZ6_jB_wFOmtVgCdAT9v1YoVJMWNq5stMYd-_AxCvlipTkJXc=w1263-h593" alt="imagen del logo" />
      </div>
      <div> {/* normas */}
+      const <img src="" alt="redondo morado" />
       <h2 className='text-xl font-black text-right text-white'>Normas de convivencia en el lugar del evento</h2>
        <p className='text-sm font-light text-right text-white'>*Respeto: Los participantes deberán tratarse con respeto. No se tolerarán actitudes agresivas, discriminatorias o despectivas. Puntualidad: Los participantes deben llegar a tiempo. La llegada tardía puede resultar en descalificación. Comportamiento: Se espera un comportamiento adecuado en todo momento. El uso de lenguaje ofensivo o inapropiado      (prohibido). Alimentos y Bebidas: No se permiten alimentos ni bebidas en la sala de informática.</p>
      </div> {/* parte de todos los juegos */}
@@ -18,12 +21,16 @@ const InfoSection = () => {
       {/* todos los h3 son los juegos */}
        <h3 className='text-lg font-bold text-right text-white'>free fire(mobile)</h3>
         <p className='text-sm font-medium text-right text-[#58FCEC]'>Free Fire es un juego de batalla real desarrollado por Garena, donde los jugadores se lanzan en paracaídas a una isla desierta, buscando armas y suministros para sobrevivir. Los jugadores deben eliminar a los demás mientras el área de juego se va reduciendo, obligándolos a enfrentarse. El último jugador o equipo en pie gana la partida. Con un estilo de juego rápido y dinámico, Free Fire ofrece diversas modalidades y personajes, cada uno con habilidades únicas.</p>
+       const <img src="" alt="imagen free fire" />
        <h3 className='text-lg font-bold text-left text-white'>Counter-Strike 1.6 No Steam(PC)</h3>
         <p className='text-sm font-medium text-left text-[#58FCEC]'>Counter-Strike es un clásico juego de disparos en primera persona (FPS) que se centra en el combate en equipo. Los jugadores se dividen en dos equipos: los terroristas (T) y los antiterroristas (CT). Cada equipo tiene objetivos específicos, como colocar o desactivar una bomba o rescatar rehenes. El juego destaca por su enfoque táctico, estrategia en equipo y un sistema de economía que permite a los jugadores comprar armas y equipo entre rondas. </p>
+       const <img src="" alt="imagen counter strike" />
        <h3 className='text-lg font-bold text-right text-white'>Browl Star(mobile)</h3>
         <p className='text-sm font-medium text-right text-[#58FCEC]'>Brawl Stars es un juego de acción multijugador en línea desarrollado por Supercell. En él, los jugadores controlan personajes llamados "Brawlers", cada uno con habilidades y ataques únicos. El juego presenta varias modalidades, desde enfrentamientos 3v3 hasta batallas individuales, donde los jugadores compiten por recolectar estrellas, eliminar enemigos y cumplir objetivos. Con un estilo vibrante y dinámico, Brawl Stars combina estrategia, trabajo en equipo y un ritmo rápido.</p>
+       const <img src="" alt="imagen browl star" />
        <h3 className='text-lg font-bold text-left text-white'>Tetr.io(PC)</h3>
         <p className='text-sm font-medium text-left text-[#58FCEC]'>Tetr.io es un juego de rompecabezas en línea que se inspira en el clásico Tetris, ofreciendo una experiencia moderna y competitiva. Los jugadores deben encajar piezas de diferentes formas (tetrominós) en una cuadrícula para formar líneas horizontales completas, que luego desaparecen. A medida que se eliminan líneas, el juego se vuelve más rápido y desafiante</p>
+       const <img src="" alt="imagen tetris" />
      </div>
     </div>
   )
